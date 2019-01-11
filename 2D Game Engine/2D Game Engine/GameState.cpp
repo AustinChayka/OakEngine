9bbbd3babe2d75b7e4e4f571d5c55559898188d1
Austin Chayka <austinchayka@gmail.com>
2019-01-11 13:58:44 -0800

    Removed Old Files
    Removed old gameState and level classes

--- 2D Game Engine/2D Game Engine/GameState.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameState.h"
-
-GameState::GameState(char * init_stateName) {
-
-	stateName = init_stateName;
-
-}
-
-GameState::~GameState() {}
--- 2D Game Engine/2D Game Engine/TestObject.cpp
+++ 2D Game Engine/2D Game Engine/TestObject.cpp
@@ -1,8 +1,8 @@
 #include "TestObject.h"
 
-TestObject::TestObject() : GameObject("Test.png", 10, 10, 32, 32, 3) {
+TestObject::TestObject() : GameObject("assets/Test.png", 10, 10, 32, 32, 5) {
 
-	vX = 1;
+	vX = 1.5f;
 	vY = 1;
 
 }
@@ -18,5 +18,7 @@ void TestObject::Update() {
 
 	x += vX;
 	y += vY;
+
+	scale -= .01f;
 
 }
